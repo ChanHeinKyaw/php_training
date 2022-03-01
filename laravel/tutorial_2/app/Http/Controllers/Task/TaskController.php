6f420839514bcd7bd7e81bd7ba1a5f4d4a5bbff4
Chan Hein kyaw <chanheinkyaw.mm@gmail.com>
2022-03-01 19:39:52 +0630

    master

---
+++ laravel/tutorial_2/app/Http/Controllers/Task/TaskController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers\Task;
+
+use App\Task;
+use App\Http\Requests\TaskRequest;
+use App\Http\Controllers\Controller;
+use App\Contracts\Services\Task\TaskServiceInterface;
+
+class TaskController extends Controller
+{
+  private $taskInterface;
+
+  public function __construct(TaskServiceInterface $taskServiceInterface)
+  {
+    $this->taskInterface = $taskServiceInterface;
+  }
+
+    public function index(){
+        $tasks = $this->taskInterface->getTaskList();
+        return view('tasks', compact('tasks'));
+    }
+
+    public function store(TaskRequest $request){
+        $task = $this->taskInterface->saveTask($request);
+        return redirect('/');
+    }
+
+    public function destroy($id){
+        $msg = $this->taskInterface->deleteTaskById($id);
+        return redirect('/');
+    }
+}
